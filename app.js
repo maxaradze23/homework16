f66c401a8f8d6e7d4ab808e603da12fcd4155710
maxaradze23 <119763321+maxaradze23@users.noreply.github.com>
2023-01-27 00:00:19 -0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,81 @@
+// 1 
+
+function isOdd(n) {
+    return n % 2 === 0;
+  }
+  console.log(isOdd(10));
+  
+  // 2
+
+  // function getCurrencySymbolFromCode(Currency) {
+  //   if (Currency === "USD") {
+  //     console.log("$");
+  //     return "$";
+  //   }
+  //   if (Currency === "EUR") {
+  //     console.log("€");
+  //     return "€";
+  //   }
+  //   if (Currency === "GEL") {
+  //     console.log("ლ");
+  //     return "ლ";
+  //   }
+  //   return;
+  // }
+  // getCurrencySymbolFromCode("USD");
+  // getCurrencySymbolFromCode("EUR");
+  // getCurrencySymbolFromCode("GEL");
+  
+  // //  3
+  
+  // function convertToLowerCase(str) {
+  //   let lowerCaseString = str.toLowerCase();
+  //   console.log(lowerCaseString);
+  //   return lowerCaseString;
+  // }
+  
+  // convertToLowerCase("MY NAME IS lado");
+  
+  // //  4
+  
+  // function filterEvenNumbers(numbers) {
+  //   return numbers.filter((num) => num % 2 === 0);
+  // }
+  
+  // console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+  
+  // //  5
+  
+  // function findObjectByName(objects, name) {
+  //   return objects.find((object) => object.name === name);
+  // }
+  
+  // let objects = [
+  //   {
+  //     name: "salome",
+  //     age: 28,
+  //   },
+  //   {
+  //     name: "zaza",
+  //     age: 27,
+  //   },
+  //   {
+  //     name: "lado",
+  //     age: 20,
+  //   },
+  // ];
+  // console.log(findObjectByName(objects, "zaza"));
+  
+  // // 6
+  
+  // function CalcExpense(income, expense) {
+  //   const profit = income - expense;
+  //   return profit;
+  // }
+  // let jan = CalcExpense(100, 30);
+  // let feb = CalcExpense(180, 70);
+  // let mar = CalcExpense(250, 150);
+  
+  // console.log(jan);
+  // console.log(feb);
+  // console.log(mar);
